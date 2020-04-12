47d266afbfc72bbd4523e919c00b27d0beb91a92
Joerg Drechsler <lept-tess-ocv@medaso.de>
2020-04-12 10:35:00 +0200

    improve meta build script: make grep case insensitive

--- lept-tess-ocv-build_piano.sh
+++ lept-tess-ocv-build_piano.sh
@@ -4,4 +4,4 @@
 
 clear && \
     time ./lept-tess-ocv-build.sh ${1} \
-        | grep --color=always 'error\|c4013\|warning\|vcxproj\|configuring'
+        | grep -i --color=always 'error\|C4013\|warning\|vcxproj\|configuring'
