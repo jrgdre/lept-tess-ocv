60b90fd1df82d6aefc92b2002c4773c37f8a328d
Joerg Drechsler <lept-tess-ocv@medaso.de>
2020-04-15 11:22:19 +0200

    fix meta script: forward all parameters to build script

--- lept-tess-ocv-build_piano.sh
+++ lept-tess-ocv-build_piano.sh
@@ -1,7 +1,21 @@
 #!/bin/bash -e
 
+## Leptonica, Tesseract, OpenCV build
 # run the build script with reduced noise
+#
+# usage:
+#   s. ReadMe.md
+#
+# (c)2020 Medical Data Solutions GmbH
+# License: MIT (s. License.md)
+#
+# authors:
+#   jrgdre  "Joerg Drechsler, Medical Data Solutions GmbH"
+#
+# versions:
+#   1.0.0 2020-03-26 jrgdre "initial release"
 
 clear && \
-    time ./lept-tess-ocv-build.sh ${1} \
-        | grep -iw --color=always 'error\|C4013\|warning\|vcxproj\|configuring'
+    time ./lept-tess-ocv-build.sh $@ \
+        | grep -iw --color=always \
+			'error\|C4013\|warning\|vcxproj\|configuring'
